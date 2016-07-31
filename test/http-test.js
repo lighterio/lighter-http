bbf3166524eaedf7464802918edc2900066dbd8c
Sam Eubank <sameubank+zerious@gmail.com>
2016-07-31 06:22:42 -0700

    Fixed a library path

--- test/http-test.js
+++ test/http-test.js
@@ -2,7 +2,7 @@ var http = require('http')
 var is = global.is || require('exam/lib/is')
 var port = 31267
 var url = 'http://127.0.0.1:' + port + '/'
-var lighterHttp = require('lighter-http')
+var lighterHttp = require('../lighter-http')
 
 var server = new lighterHttp.Server({port: port})
 
